0b1a219f91b4f3803b55fabe4b5e0ee0d89564ba
Yadav <S39527@u-ssi.net>
2016-11-11 13:28:15 +0000

    1

--- src/test/java/Test/Test/runners/FeatureRunner.java
+++ src/test/java/Test/Test/runners/FeatureRunner.java
@@ -3,7 +3,7 @@ package Test.Test.runners;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
  
-@CucumberOptions(features = "src/test/resources/features",
+@CucumberOptions(features = "src/test/resources/features/NewEra_433.feature",
 glue = "Test.Test.glue",
 format = { "pretty","html:target/reports",
 "json:target/cucumber.json" }, tags = {"~@ignore" })
